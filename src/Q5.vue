77d9f6c09f84e1f01bfdce6d0d186c9c090551d4
tfunk2 <tyler.j.funk2@gmail.com>
2022-08-18 15:59:37 -0600

    Q5

--- src/Q5.vue
+++ src/Q5.vue
@@ -1,15 +1,78 @@
 <script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+import NetworkTable from './components/NetworkTable.vue'
+
+const fields = [
+  {
+    key: "network_id",
+    value: "Network ID"
+  },
+  {
+    key: "score_average",
+    value: "Score Average"
+  },
+  {
+    key: "owner_id",
+    value: "Owner ID"
+  },
+  {
+    key: "network_name",
+    value: "Network Name"
+  },
+  {
+    key: "username",
+    value: "Username"
+  },
+  {
+    key: "email",
+    value: "Email"
+  },
+];
+
+const getNetwork = () => fetch('/phpEndpoint/endpoint-Q5/php-endpoint.php')
+  .then(resp => resp.json())
+  .then(data => network.value = data)
+  
+const network = ref({});
+
+const averageScores = (score1, score2, score3) => {
+  return Math.round((parseInt(score1) + parseInt(score2) + parseInt(score3)) / 3).toFixed(2);
+};
+
+const items = computed(() => {
+  return {
+    "network_id": network.value.network["network_id"],
+    "score_average": averageScores(
+      network.value.network["score1"], 
+      network.value.network["score2"], 
+      network.value.network["score3"]
+    ),
+    "owner_id": network.value.network["owner_id"],
+    "network_name": network.value.network["network_name"],
+    "username": network.value.network["username"],
+    "email": network.value.network["email"],
+  }
+})
+
+onMounted(async () => {
+  await getNetwork();
+});
+
 </script>
 
 <template>
-  <div>
-    <div class="bg-info block"></div>
-  </div>
+    <div class="container-fluid">
+      <NetworkTable
+        v-if="network !== undefined"
+        :title="network.network ? network.network.network_name : 'error'"
+        :fields="fields" 
+        :items="items" 
+      />
+    </div>
 </template>
 
 <style scoped>
-.block {
-  height: 100px;
-  width: 100px;
+.card {
+  color: black;
 }
 </style>
---
+++ src/exampleNetworkOutput.js
@@ -0,0 +1,12 @@
+export default exampleOutput = {
+  "network": {
+      "network_id": 1,
+      "score1": 1,
+      "score2": 2,
+      "score3": 3,
+      "owner_id": 1,
+      "network_name": "VNL Network",
+      "username": "Test User",
+      "email": "test@email.com"
+  }
+}
